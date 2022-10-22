305cfdf7ff38f5ac4d12c330210688c57ba2dec4
Paulina.Bury <javadevil321@gmail.com>
2022-10-22 16:13:44 +0200

    Add login/logout functionality

--- admin/index.php
+++ admin/index.php
@@ -5,6 +5,15 @@
     <div class="wrapper">
         <h1>Dashboard</h1>
 
+        </br></br>
+        <?php
+        if (isset($_SESSION['login'])) {
+            echo $_SESSION['login']; //Displaing Session login Message
+            unset($_SESSION['login']); //Removing Session login Message
+        }
+        ?>
+        </br></br>
+
         <div class="col-4 text-center">
             <h1>5</h1>
             </br>
---
+++ admin/login.php
@@ -0,0 +1,74 @@
+<?php include('../config/constants.php') ?>
+
+<html>
+
+<head>
+    <title>Login - Food Order System</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<body>
+
+    <div class="login">
+        <h1 class="text-center">Login</h1></br></br>
+
+        <?php
+        if (isset($_SESSION['login'])) {
+            echo $_SESSION['login']; //Displaing Session login Message
+            unset($_SESSION['login']); //Removing Session login Message
+        }
+        ?>
+        </br></br>
+
+        <!-- Login form starts here -->
+
+        <form action="" method="POST" class="text-center">
+            Username:</br>
+            <input type="text" name="username" placeholder="Username"></br></br>
+            Password:</br>
+            <input type="password" name="password" placeholder="Password"></br></br>
+
+            <input type="submit" name="submit" value="Login" class="btn-primary">
+        </form>
+        </br>
+        <!-- Login form ends here -->
+
+        <p class="text-center">Created by - Paulina Bury</p>
+    </div>
+
+</body>
+
+</html>
+
+<?php
+//check if submit button is clicked or not
+if (isset($_POST['submit'])) {
+    //Process loging
+    //1. Get the data from login form
+    $username = $_POST['username'];
+    $password = md5($_POST['password']);
+
+    //2. sql query to check whether the user exists
+    $sql = "SELECT * FROM tbl_admin WHERE username='$username' AND password='$password'";
+
+    //3. Execute the query
+    $res = mysqli_query($conn, $sql);
+
+    //4. Count rows to check if user exists
+    $count = mysqli_num_rows($res);
+
+    if($count == 1) {
+        //user exists
+        $_SESSION['login'] = "<div class='success'>Login successfull.</div>"; 
+        $_SESSION['user'] = $username; //to check if user is logged in or not and logout will unset it
+
+        //redirect to home page/Dashboard
+        header('location:'.SITEURL.'admin/');
+    } else {
+        //user does not exists
+        $_SESSION['login'] = "<div class='error text-center'>Wrong user name or password.</div>"; 
+        //redirect to home page/Dashboard
+        header('location:'.SITEURL.'admin/login.php');
+    }
+}
+?>
